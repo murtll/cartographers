9f1f8e52e7135a11b6abb015cf9a649a994a51e7
kashild <kozuhovura2@gmail.com>
2026-08-18 02:27:11 +0300

    fix some bugs

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -29,55 +29,51 @@ func main() {
 
 	addr := ":" + env("PORT", "8080")
 
-	mux := api.NewRouter()
-
-	srv := &http.Server{
-		Addr:              addr,
-		Handler:           mux,
-		ReadHeaderTimeout: 5 * time.Second,
-	}
-
-	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
 	// WriteTimeout здесь нет намеренно: стрим событий висит открытым всю партию,
 	// и общий таймаут на запись его бы обрывал. Сроки на отдельные запросы
 	// ставят сами ручки.
+	srv := &http.Server{
+		Addr:              addr,
+		Handler:           api.NewRouter(),
+		ReadHeaderTimeout: 5 * time.Second,
+	}
 
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
-	go getmux(log, srv, shutdownCtx)
 
-	healthzaddr := ":" + env("PORT", "9090")
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	go startSrv(log, srv, shutdownCtx, "main")
 
-	healthzmux := api.HealthzRouter()
+	healthzaddr := ":" + env("HEALTHZ_PORT", "9090")
 
 	healthzsrv := &http.Server{
 		Addr:              healthzaddr,
-		Handler:           healthzmux,
+		Handler:           api.HealthzRouter(),
 		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
-	healthzShutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+	healthzShutdownCtx, healthzCancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer healthzCancel()
 
-	go getmux(log, healthzsrv, healthzShutdownCtx)
+	go startSrv(log, healthzsrv, healthzShutdownCtx, "hehealthz")
 
 	<-ctx.Done()
 	log.Info("shutting down")
 	log.Info("healthz shutting down")
 }
 
-func getmux(log *slog.Logger, srv *http.Server, shutdownCtx context.Context) {
-	log.Info("server listening", "healthzaddr", srv.Addr)
+func startSrv(log *slog.Logger, srv *http.Server, shutdownCtx context.Context, addrName string) {
+	log.Info("server listening", addrName + "Addr", srv.Addr)
 	if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
 		log.Error("listen failed", "err", err)
 		os.Exit(1)
 	}
 
 	if err := srv.Shutdown(shutdownCtx); err != nil {
-		log.Error("healthz shutdown failed", "err", err)
+		log.Error("shutdown failed", "addrName", addrName, "err", err.Error())
 	}
 }
 
